6423198059931fa9920e2cff79898b5d99e1c97d
GOLD <Adeniran@Gold-MacBookPros-Pro.local>
2024-07-11 14:36:55 +0100

    Implemented the hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
         <Header></Header>
+        <Hero></Hero>
         <ButtonGradient></ButtonGradient>
       </div>
     </>
