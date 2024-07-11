4149cddfb35eddd2d01eabbb12e1551b6978b0bc
GOLD <Adeniran@Gold-MacBookPros-Pro.local>
2024-07-11 13:15:59 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
+        <Header></Header>
+        <ButtonGradient></ButtonGradient>
+      </div>
+    </>
+  );
+}
+
+export default App;
