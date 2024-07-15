12b778ca8f3a0d4400ac7c769378ace98eb3f04d
GOLD <Adeniran@Gold-MacBookPros-Pro.local>
2024-07-15 10:16:25 +0100

    Completed All Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -8,6 +14,12 @@ function App() {
       <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
         <Header></Header>
         <Hero></Hero>
+        <Benefits></Benefits>
+        <Collaboration></Collaboration>
+        <Services></Services>
+        <Pricing></Pricing>
+        <Roadmap></Roadmap>
+        <Footer></Footer>
         <ButtonGradient></ButtonGradient>
       </div>
     </>
--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,7 +3,7 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Tagline from "./TagLine";
 import { roadmap } from "../constants";
-import { check2, grid, loading1 } from "../../public/assets";
+import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
 
 const Roadmap = () => (
